9f2094e0dda76bb91dcec4ca7439bb87088b5bac
PernillaTheStudent <pernilla.codex@gmail.com>
2023-06-23 15:22:51 +0200

    Add create-blog functionality

--- pages/create-post/index.jsx
+++ pages/create-post/index.jsx
@@ -1,10 +1,29 @@
 import BlogEditor from "@/components/blog-editor";
 import { createSlug } from "@/utils/createSlug";
+import { useUser } from "@supabase/auth-helpers-react";
+import { useRouter } from "next/router";
 
 export default function CreatePost() {
-  const handleOnSubmit = ({ editorContent, titleInput, image }) => {
+  const router = useRouter();
+  const user = useUser();
+
+  const handleOnSubmit = async ({ editorContent, titleInput, image }) => {
     const slug = createSlug(titleInput);
+
     console.log({ editorContent, titleInput, image, slug });
+
+    const newPost = {
+      body: editorContent,
+      title: titleInput,
+      slug,
+      user_id: user.id,
+    }
+
+    const { error } = await addPostTrigger(newPost);
+
+    if (!error) {
+      router.push(`/blog/${slug}`);
+    }
   };
 
   return (
